3c9cee1216269172ee4ef67ae96e4852c99a0dfb
themisbrink <themisbrnk@gmail.com>
2021-06-04 18:01:13 +0300

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,7 +5,7 @@ export const environment = {
   
     // the path to the game server for the game DataService calls
     // gameServerDomainUrl: 'https://swipewin-whitelabel.herokuapp.com',
-    gameServerDomainUrl: 'https://swipewin-mtnsa-server.herokuapp.com',
+    gameServerDomainUrl: 'https://swipe-bok-internal-server.herokuapp.com',
     
     // the path to MTN consent pages
     // mtnAuthDomainUrl: 'http://staging.doi.mtndep.co.za/service/43954',
